d0b02f94fbcd1f44331bbe70b4d9f9378f35f635
benjaminhr <robsonhbenjamin@gmail.com>
2022-01-04 18:59:27 +0200

    cut dark colour strips out

--- border.py
+++ border.py
@@ -2,6 +2,8 @@ import cv2
 import pathlib
 import glob
 import os
+import numpy as np
+from numpy.linalg import norm
 
 OUTPUT_FOLDER = str(pathlib.Path("./output-border").resolve())
 PHOTO_FOLDER = str(pathlib.Path("./output-colours").resolve())
@@ -25,13 +27,31 @@ month_mappings = {
     "12": "December",
 }
 
+images = []
+
 for photo_path in photo_files:
     img = cv2.imread(photo_path)
+    images.append({
+        "path": photo_path,
+        "image": img
+    })
+
+images.sort(key=lambda img: np.average(
+    norm(img["image"], axis=2)) / np.sqrt(3))
+
+print(len(images))
+remove = 300
+images = images[remove:]
+print(len(images))
+
+for imageObject in images:
+    image = imageObject["image"]
+    path = imageObject["path"]
 
     img = cv2.copyMakeBorder(
-        img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+        image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
 
-    filename = os.path.basename(photo_path)
+    filename = os.path.basename(path)
     month_taken = month_mappings[filename[:2]]
     font = cv2.FONT_HERSHEY_TRIPLEX
     textsize = cv2.getTextSize(month_taken, font, 1, 2)[0]
@@ -39,8 +59,5 @@ for photo_path in photo_files:
 
     cv2.putText(img, month_taken, (textX, 390), font,
                 2, (0, 0, 0), 2, cv2.LINE_AA)
-
-    # cv2.putText(img, month_taken, (400, 390), font,
-    #             2, (0, 0, 0), 2, cv2.LINE_AA)
 
     cv2.imwrite(f"{OUTPUT_FOLDER}/{filename}", img)
